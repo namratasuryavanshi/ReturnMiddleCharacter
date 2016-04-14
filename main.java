5c82ccdd2b687b6cd2e208d3cad4275fbc53f9f7
Namrata <namratasuryavanshi@users.noreply.github.com>
2016-04-13 19:24:20 -0700

    Update main.java
    added notes

--- main.java
+++ main.java
@@ -1,8 +1,17 @@
+/*
+return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+Examples:
+ReturnMiddleCharacter.getMiddle("test") should return "es"
+ReturnMiddleCharacter.getMiddle("testing") should return "t"
+ReturnMiddleCharacter.getMiddle("middle") should return "dd"
+ReturnMiddleCharacter.getMiddle("A") should return "A"
+*/
+
 import java.util.Scanner;
 import java.lang.String;
 
 
-class Kata {
+class ReturnMiddleCharacter {
   public static String getMiddle(String word) {
       if(word.length() % 2 == 0 ){
          word = word.substring((word.length() / 2) - 1, (word.length() / 2) + 1);
